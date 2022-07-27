959b26989cd7d0386b70e2ade041dcfab3cfc533
Carol Wanjohi <wanjohi.carol54@gmail.com>
2022-07-27 14:26:35 +0300

    add assets, component info and remove nc-welcome page

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { getAllGames } from '../fake-api';
 
 @Component({
   selector: 'bg-hoard-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'store';
+  title = 'Board Game Hoard';
+  games = getAllGames();
 }
--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    MatCardModule, 
   ],
   providers: [],
+  declarations: [AppComponent,],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
